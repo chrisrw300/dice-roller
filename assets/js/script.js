d003cd9ba5c740f192a497caba6897a58da2a46e
Chris Walston <Chris@myaccountant.local>
2021-02-02 11:17:00 -0700

    added if statement to display each die

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,47 @@ function numberGen() {
     let diceNumber2 = random2.toFixed();
 
     console.log(`${diceNumber1} ${diceNumber2}`);
+
+    displayDice(diceNumber1, diceNumber2);
 }
 
 // display dice based on rolls
+function displayDice(diceNumber1, diceNumber2) {
+    
+    // dice 1 rolls
+    if (diceNumber1 === '1') {
+        console.log('1');
+    } else if (diceNumber1 === '2') {
+        console.log('2')
+    } else if (diceNumber1 === '3') {
+        console.log('3')
+    } else if (diceNumber1 === '4') {
+        console.log('4')
+    } else if (diceNumber1 === '5') {
+        console.log('5')
+    } else if (diceNumber1 === '6') {
+        console.log('6')
+    } else {
+        console.log('no')
+    }
 
+    // dice 2 rolls
+    if (diceNumber2 === '1') {
+        console.log('1');
+    } else if (diceNumber2 === '2') {
+        console.log('2')
+    } else if (diceNumber2 === '3') {
+        console.log('3')
+    } else if (diceNumber2 === '4') {
+        console.log('4')
+    } else if (diceNumber2 === '5') {
+        console.log('5')
+    } else if (diceNumber2 === '6') {
+        console.log('6')
+    } else {
+        console.log('no')
+    }
+}
 
 
 // add styles to dice if snake eyes is rolled
