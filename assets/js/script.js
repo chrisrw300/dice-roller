b98841a06902f5c1445f8800382df4249d7f025d
Chris Walston <Chris@myaccountant.local>
2021-02-02 12:52:59 -0700

    added feature for snake eyes and such

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // variables
 let diceContainer = document.querySelector('#dice-container');
-const btnGroup = document.querySelector('#btn-group')
+let snakeEyes = document.querySelector('.snake-eyes');
+let diceTotal = document.querySelector('#dice-total');
 const btn = document.querySelector('#btn');
-
 
 // number generator
 function numberGen() {
@@ -19,10 +19,29 @@ function numberGen() {
 
 // display dice based on rolls
 function displayDice(diceNumber1, diceNumber2) {
-    
     const dice1 = document.querySelector('#dice-1').setAttribute("src", "./assets/img/dice-" + diceNumber1 + '.png');
     const dice2 = document.querySelector('#dice-2').setAttribute("src", "./assets/img/dice-" + diceNumber2 + '.png');;
     
+    // if snake eyes are rolled, display box shadow
+    if (diceNumber1 === diceNumber2) {
+        document.querySelector('#dice-1').style.boxShadow = "0 0 25px red"
+        document.querySelector('#dice-2').style.boxShadow = "0 0 25px red"
+
+        snakeEyes.classList.remove('hidden');
+
+    } else {
+        document.querySelector('#dice-1').style.boxShadow = "none"
+        document.querySelector('#dice-2').style.boxShadow = "none"
+
+        snakeEyes.classList.add('hidden');
+    }
+
+    // parse string to add up as integers
+    let totalNumber1 = parseInt(diceNumber1, 10);
+    let totalNumber2 = parseInt(diceNumber2, 10);
+
+    // total of dice
+    diceTotal.innerHTML = totalNumber1 + totalNumber2;
 }
 // function start
 btn.addEventListener('click', numberGen);
