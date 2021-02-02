eeee983fa47bd4fea79c1ab3e0ead7eacb975943
Chris Walston <Chris@myaccountant.local>
2021-02-02 10:30:23 -0700

    added number generation js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,23 @@
 
 
 // number generator
+function numberGen() {
+    let random1 = Math.random()*5 + 1;
+    let diceNumber1 = random1.toFixed();
 
+    let random2 = Math.random()*5 + 1;
+    let diceNumber2 = random2.toFixed();
 
+    console.log(`${diceNumber1} ${diceNumber2}`);
+}
 
 // display dice based on rolls
 
 
 
 // add styles to dice if snake eyes is rolled
+
+
+
+// function start
+numberGen();
